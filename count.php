c6f107d9bb5a9d0528327593c9fd84908b1aed2f
LokmanHossainPappu <lokmanhossainpappu@gmail.com>
2025-11-04 11:19:43 +0600

    something are changed in file

--- count.php
+++ count.php
@@ -5,6 +5,8 @@ $file = basename( $_SERVER['PHP_SELF'] );
 // Count the number of lines in the current PHP script file
 $no_of_lines = count( file( $file ) );
 
+echo "Helo php";
+
 // Display the result indicating the number of lines in the file
 echo "There are $no_of_lines lines in $file" . "\n";
 ?>
